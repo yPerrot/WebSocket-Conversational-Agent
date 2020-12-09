421a881c6d7d8b5d77ec8ad54f9f511512d0d086
unknown <yperrot@outlook.com>
2020-12-09 13:50:26 +0100

    Implement basic feature

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var RiveScript = require("rivescript");
+
+var bot = new RiveScript();
+
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
@@ -9,11 +14,37 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log("New connection")
+
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+        
+      bot.sortReplies();
+      
+      let username = "local-user";
+
+      bot.reply(username, msg).then(function(reply) {
+        if (reply == "ERR: No Reply Matched") {
+          reply = "BrrBrr, BOT don't know what to say"
+        }
+        io.emit('chat message', "BOT : " + reply);
+      });
+
+
     });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+bot.loadFile("./brain.rive").then(loading_done).catch(loading_error);
+
+
+function loading_done() {
+  console.log("Bot has finished loading!");
+
+  http.listen(port, function(){
+    console.log('listening on *:' + port);
+  });
+
+}
+ 
+// It's good to catch errors too!
+function loading_error(error, filename, lineno) {
+  console.log("Error when loading files: " + error);
+}
